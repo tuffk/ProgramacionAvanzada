3d3a31d3fa4d0ca472234dd2ac0d21dfdbc90e5d
Tuff <tuff@localhost.localdomain>
2015-11-05 15:54:39 -0600

    ejemplos de open mp parallel

---
+++ clases/ej1openmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 10000000
+int main()
+{
+  long long int tid,i,suma;
+  suma=0;
+  double kuz,zain;
+  kuz = omp_get_wtime(); 
+#pragma omp parallel private(tid)
+  {
+    //tid=omp_get_thread_num();
+#pragma omp for private(i) reduction(+:suma) schedule(dynamic,100000)
+    for(i=0;i<N;++i)
+    {
+//#pragma omp atomic
+      suma+=i;
+    }
+  }
+  zain=omp_get_wtime(); 
+  printf("%lli y tarde %f\n",suma,(zain-kuz));
+  
+  return 0;
+}
